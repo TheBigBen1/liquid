4be20b3d6d5802574f5e6f29ca106ee3831023c7
BigBen <42361307+TheBigBen1@users.noreply.github.com>
2024-03-27 14:10:15 +0000

    added build instructions

---
+++ build_instructions.sh
@@ -0,0 +1,8 @@
+pip wheel . -w dist
+mkdir -p python-lib/python
+WHEEL_FILE=$(find dist -name 'python_liquid*.whl' -print -quit)
+pip install "$WHEEL_FILE" -t python-lib/python
+cd python-lib
+zip -r python-liquid-layer.zip .
+cd ..
+pip install $WHEEL_FILE
